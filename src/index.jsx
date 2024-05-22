d0826a4ac6189101ec92584531c28c6616332590
Jimmy Liu <jmliu@yuansuan.cn>
2024-05-22 17:57:21 +0800

    feat: add Class Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 // Forked from https://reactjs.org/tutorial/tutorial.html#what-are-we-building
 
 import React from './mini-react';
-import App from './App';
+import App from './App'; // LegacyClassApp
 import './styles.css';
 
 React.render(<App />, document.getElementById('root'));
